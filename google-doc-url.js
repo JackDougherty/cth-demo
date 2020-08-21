1539e2b7953e063f2a3c28f27d4e995a938eb446
Jack Dougherty <jack.dougherty@trincoll.edu>
2020-08-21 13:00:38 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QY6ifBl7rIGbw2PsoySFacEGoytVyGc3XEJmK1K9drs/edit#gid=0';
